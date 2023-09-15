30df16bf96a38d7d77a4ea854cad31d6937f33d1
Juice <akgnerf@gmail.com>
2023-09-15 19:50:37 +0530

    modified:   quicksort.py

---
+++ lec3/traversal.py
@@ -0,0 +1,7 @@
+# repping the graph - create a list of nodes, and then make an AM
+# what is a node - value and what nodes are adjacent. 
+
+nodes = [A,B,C,D,E,F,G,H,I,J,K,L]
+
+matrix = []
+
--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -1,9 +1,11 @@
+import random 
+
 def partition(L,start,stop):
     pivot = L[start]
     wall = start
 
     for scout in range(start+1,stop+1):
-        if L[scout] < L[pivot]:
+        if L[scout] < pivot:
             wall = wall + 1
             L[wall] , L[scout] = L[scout] , L[wall]
     
@@ -17,6 +19,15 @@ def quicksort(L,start,stop):
     quicksort(L,start,p-1)
     quicksort(L,p+1, stop)
 
+def main():
+    list = [num for num in range(20,40)]
+    random.shuffle(list)
+    print(list)
+    quicksort(list,0,19)   
+    print(list) 
+
+
+main()
 
 
 
