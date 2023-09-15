c3fa4d9ce0ff48116d9ac7ba9be3e081cd6455e0
Juice <akgnerf@gmail.com>
2023-09-15 19:59:44 +0530

    Changes to be committed:

--- lec3/traversal.py
+++ lec3/traversal.py
@@ -1,7 +1,8 @@
 # repping the graph - create a list of nodes, and then make an AM
 # what is a node - value and what nodes are adjacent. 
 
-nodes = [A,B,C,D,E,F,G,H,I,J,K,L]
+nodes = [string for string in range(26,30)]
+print(nodes)
 
 matrix = []
 
