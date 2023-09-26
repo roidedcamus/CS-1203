72045922b34e0b68f91c7bff4a065ba5cd3bd07d
Juice <akgnerf@gmail.com>
2023-09-26 17:57:11 +0530

    modified:   lec3/traversal.c 	new file:   pset1/pset.c

--- lec3/traversal.c
+++ lec3/traversal.c
@@ -1,4 +1,36 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-//work with 10 nodes - a to k
+typedef char* string;
 
+//create maze using pointer nodes
+//intitialize AM to all false and parents array to all -1
+//define stack operations
+//traverse function - takes root as input, maintains a stack - pops top, adds adjacent of popped( if p[i] == -1), also updates AM)
+// repeats till dead end/exit reached, then returns top of stack
+// show path using returned value and AM + array
+
+#define N 10;
+bool matrix[N][N];
+int parents[N];
+
+void main()
+{
+
+}
+
+void start(bool am[][], int p[])
+{
+    for (int i = 0; i < N; i++)
+    {
+        p[i] = -1;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrix[i][j] = F;
+        }
+    }
+}
